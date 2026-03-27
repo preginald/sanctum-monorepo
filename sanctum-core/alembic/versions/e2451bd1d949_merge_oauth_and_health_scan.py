c12d179686a912ef08f56e445819cadde84e98ef
preginald <peter@reginald.com.au>
2026-03-28 09:07:15 +1100

    #1099: fix merge migration to point to actual branch tip d7f2e9b34c12

--- sanctum-core/alembic/versions/e2451bd1d949_merge_oauth_and_health_scan.py
+++ sanctum-core/alembic/versions/e2451bd1d949_merge_oauth_and_health_scan.py
@@ -1,7 +1,7 @@
 """merge_oauth_and_health_scan
 
 Revision ID: e2451bd1d949
-Revises: 2584e253fb63, a2900e459652
+Revises: 2584e253fb63, d7f2e9b34c12
 Create Date: 2026-03-28 08:40:47.978835
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'e2451bd1d949'
-down_revision: Union[str, None] = ('2584e253fb63', 'a2900e459652')
+down_revision: Union[str, None] = ('2584e253fb63', 'd7f2e9b34c12')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
