30db46e160f46bd2473f63b56835dd288361d050
preginald <peter@reginald.com.au>
2026-03-28 08:32:58 +1100

    #1099: fix duplicate revision ID in oauth client migration

--- sanctum-core/alembic/versions/2584e253fb63_add_oauth_client_id_to_accounts.py
+++ sanctum-core/alembic/versions/2584e253fb63_add_oauth_client_id_to_accounts.py
@@ -1,6 +1,6 @@
 """add oauth_client_id to accounts
 
-Revision ID: a1b2c3d4e5f6
+Revision ID: 2584e253fb63
 Revises: ec65c8baf023
 Create Date: 2026-03-28 10:00:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'a1b2c3d4e5f6'
+revision: str = '2584e253fb63'
 down_revision: Union[str, None] = 'ec65c8baf023'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
